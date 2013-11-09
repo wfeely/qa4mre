293ee7f0d1463bc6f43cdaca4c5ac39b98b89e2a
wfeely <wfeely@cs.cmu.edu>
2013-11-09 15:58:52 -0500

    added VerbPhrase type, added VerbPhrase annotation to PhraseAnnotator

--- qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/annotators/PhraseAnnotator.java
+++ qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/annotators/PhraseAnnotator.java
@@ -1,3 +1,8 @@
+/**
+ * PhraseAnnotator.java
+ * Annotates NounPhrases and VerbPhrases
+ * @author: Weston Feely
+ */
 package edu.cmu.lti.deiis.hw5.annotators;
 
 import java.util.ArrayList;
@@ -13,6 +18,7 @@ import edu.cmu.lti.qalab.types.NounPhrase;
 import edu.cmu.lti.qalab.types.Sentence;
 import edu.cmu.lti.qalab.types.TestDocument;
 import edu.cmu.lti.qalab.types.Token;
+import edu.cmu.lti.qalab.types.VerbPhrase;
 import edu.cmu.lti.qalab.utils.Utils;
 
 public class PhraseAnnotator extends JCasAnnotator_ImplBase {
@@ -34,10 +40,17 @@ public class PhraseAnnotator extends JCasAnnotator_ImplBase {
 
       Sentence sent = sentenceList.get(i);
       ArrayList<Token> tokenList = Utils.getTokenListFromSentenceList(sent);
+      // get noun phrases
       ArrayList<NounPhrase> phraseList = extractNounPhrases(tokenList, aJCas);
       FSList fsPhraseList = Utils.createNounPhraseList(aJCas, phraseList);
       fsPhraseList.addToIndexes(aJCas);
       sent.setPhraseList(fsPhraseList);
+      // get verb phrases
+      ArrayList<VerbPhrase> verbPhraseList = extractVerbPhrases(tokenList, aJCas);
+      FSList fsVerbPhraseList = Utils.createVerbPhraseList(aJCas, verbPhraseList);
+      fsVerbPhraseList.addToIndexes(aJCas);
+      sent.setVerbPhraseList(fsVerbPhraseList);
+      // add sentence to indexes
       sent.addToIndexes();
       sentenceList.set(i, sent);
     }
@@ -57,11 +70,12 @@ public class PhraseAnnotator extends JCasAnnotator_ImplBase {
       String pos = token.getPos();
 
       // Build a noun phrase by matching POS tags
-      // Group5 changes: added adjective variants (JJR, JJS), noun variants (NNS, NNP, NNPS) and
-      // determiners (DT)
+      // Group5 changes: added determiners, pronouns, possessive pronouns, possessive markers,
+      // adverbs to list of allowable NP POS tags
+      // TODO: allow prepositions (IN & TO) if at least one noun has already been added
       if (pos.startsWith("NN") || pos.startsWith("JJ") || pos.startsWith("CD")
-              || pos.startsWith("JJR") || pos.startsWith("JJS") || pos.startsWith("NNS")
-              || pos.startsWith("NNP") || pos.startsWith("NNPS") || pos.startsWith("DT")) {
+              || pos.startsWith("DT") || pos.startsWith("PDT") || pos.startsWith("RB")
+              || pos.startsWith("PRP") || pos.startsWith("POS")) {
         nounPhrase += word + " ";
       } else {
         nounPhrase = nounPhrase.trim();
@@ -83,6 +97,43 @@ public class PhraseAnnotator extends JCasAnnotator_ImplBase {
     }
 
     return nounPhraseList;
+  }
+
+  public ArrayList<VerbPhrase> extractVerbPhrases(ArrayList<Token> tokenList, JCas jCas) {
+
+    ArrayList<VerbPhrase> verbPhraseList = new ArrayList<VerbPhrase>();
+    String verbPhrase = "";
+    for (int i = 0; i < tokenList.size(); i++) {
+      Token token = tokenList.get(i);
+      String word = token.getText();
+      String pos = token.getPos();
+
+      // Build a verb phrase by matching POS tags
+      // Group5 changes: added verbs, modals, particles, adverbs
+      // TODO: allow prepositions (IN & TO) if at least one verb has already been added
+      if (pos.startsWith("VB") || pos.startsWith("MD") || pos.startsWith("RP")
+              || pos.startsWith("RB")) {
+        verbPhrase += word + " ";
+      } else {
+        verbPhrase = verbPhrase.trim();
+        if (!verbPhrase.equals("")) {
+          VerbPhrase vv = new VerbPhrase(jCas);
+          vv.setText(verbPhrase);
+          verbPhraseList.add(vv);
+          // System.out.println("VerbPhrase: "+verbPhrase);
+          verbPhrase = "";
+        }
+      }
+
+    }
+    verbPhrase = verbPhrase.trim();
+    if (!verbPhrase.equals("")) {
+      VerbPhrase vv = new VerbPhrase(jCas);
+      vv.setText(verbPhrase);
+      verbPhraseList.add(vv);
+    }
+
+    return verbPhraseList;
   }
 
 }
