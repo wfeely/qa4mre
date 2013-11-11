6f8e9c90c86651444e844bcd5cc57ed182597e6a
Siping <hotcoderj@gmail.com>
2013-11-10 20:44:41 -0500

    update indexer to add verb phrases of sentences into index

--- qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/indexers/SolrIndexer.java
+++ qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/indexers/SolrIndexer.java
@@ -19,127 +19,138 @@ import edu.cmu.lti.qalab.types.Dependency;
 import edu.cmu.lti.qalab.types.NER;
 import edu.cmu.lti.qalab.types.NounPhrase;
 import edu.cmu.lti.qalab.types.Sentence;
-import edu.cmu.lti.qalab.types.Synonym;
 import edu.cmu.lti.qalab.types.TestDocument;
+import edu.cmu.lti.qalab.types.VerbPhrase;
 import edu.cmu.lti.qalab.utils.Utils;
 
 public class SolrIndexer extends JCasAnnotator_ImplBase {
 
-	int mDocNum;
-	File mOutputFile = null;
-	SolrWrapper wrapper;
-	String serverUrl;
-	IndexSchema indexSchema;
-	String coreName;
-	String schemaName;
+  int mDocNum;
 
-	double THRESHOLD = 4.0;
+  File mOutputFile = null;
 
-	@Override
-	public void initialize(UimaContext context)
-			throws ResourceInitializationException {
-		serverUrl = (String) context.getConfigParameterValue("SOLR_SERVER_URL");
-		coreName = (String) context.getConfigParameterValue("SOLR_CORE");
-		schemaName = (String) context.getConfigParameterValue("SCHEMA_NAME");
+  SolrWrapper wrapper;
 
-		try {
-			this.wrapper = new SolrWrapper(serverUrl + coreName);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+  String serverUrl;
 
-	}
+  IndexSchema indexSchema;
 
-	@Override
-	public void process(JCas jCas) throws AnalysisEngineProcessException {
+  String coreName;
 
-		TestDocument testDoc = Utils.getTestDocumentFromCAS(jCas);// (TestDocument)
-																	// jCas.getAnnotationIndex(TestDocument.type);
+  String schemaName;
 
-		try {
-			// try to get indexschema so that you can know the fields available
-			indexSchema = SolrUtils.getIndexSchema(serverUrl, coreName,
-					schemaName);
+  double THRESHOLD = 4.0;
 
-			String id = testDoc.getId();
+  @Override
+  public void initialize(UimaContext context) throws ResourceInitializationException {
+    serverUrl = (String) context.getConfigParameterValue("SOLR_SERVER_URL");
+    coreName = (String) context.getConfigParameterValue("SOLR_CORE");
+    schemaName = (String) context.getConfigParameterValue("SCHEMA_NAME");
 
-			ArrayList<Sentence> sentenceList = Utils.fromFSListToCollection(
-					testDoc.getSentenceList(), Sentence.class);
+    try {
+      this.wrapper = new SolrWrapper(serverUrl + coreName);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
-			
-			//ArrayList<String>synonymList=new ArrayList<String>();
-			for (int i = 0; i < sentenceList.size(); i++) {
-				Sentence sent = sentenceList.get(i);
-				String sentText = sent.getText();
-				String sentId = id + "_" + i;
-				HashMap<String, Object> indexMap = new HashMap<String, Object>();
-				indexMap.put("docid", id);
-				indexMap.put("id", sentId);
-				indexMap.put("text", sentText);
+  }
 
-				FSList fsNounList = sent.getPhraseList();
-				ArrayList<NounPhrase> nounPhrases = Utils
-						.fromFSListToCollection(fsNounList, NounPhrase.class);
-				ArrayList<String> nnList = new ArrayList<String>();
-				for (int j = 0; j < nounPhrases.size(); j++) {
-					//NounPhrase nnPhr=nounPhrases.get(j);
-					nnList.add(nounPhrases.get(j).getText());
-					/*ArrayList<Synonym>synList=Utils.fromFSListToCollection(nnPhr.getSynonyms(),Synonym.class);
-					for(int k=0;k<synList.size();k++){
-						synonymList.add(synList.get(k).getText());
-					}*/
-				}
+  @Override
+  public void process(JCas jCas) throws AnalysisEngineProcessException {
 
-				indexMap.put("nounphrases", nnList);
+    TestDocument testDoc = Utils.getTestDocumentFromCAS(jCas);// (TestDocument)
+    // jCas.getAnnotationIndex(TestDocument.type);
 
-				FSList fsNEList = sent.getNerList();
-				ArrayList<NER> namedEntities = Utils.fromFSListToCollection(
-						fsNEList, NER.class);
-				ArrayList<String> neList = new ArrayList<String>();
-				for (int j = 0; j < namedEntities.size(); j++) {
-					//NER ner=namedEntities.get(j);
-					neList.add(namedEntities.get(j).getText());
-					/*ArrayList<Synonym>synList=Utils.fromFSListToCollection(ner.getSynonyms(),Synonym.class);
-					for(int k=0;k<synList.size();k++){
-						synonymList.add(synList.get(k).getText());
-					}*/
-				}
-				indexMap.put("namedentities", neList);
-				//indexMap.put("synonyms",synonymList);
+    try {
+      // try to get indexschema so that you can know the fields available
+      indexSchema = SolrUtils.getIndexSchema(serverUrl, coreName, schemaName);
 
-				FSList fsDependencies = sent.getDependencyList();
-				if (fsDependencies != null) {
-				  ArrayList<Dependency> dependencies = Utils
-				          .fromFSListToCollection(fsDependencies,
-				                  Dependency.class);
-				  ArrayList<String> depList = new ArrayList<String>();
-				  for (int j = 0; j < dependencies.size(); j++) {
-				    String rel = dependencies.get(j).getRelation();
-				    String gov = dependencies.get(j).getGovernor().getText();
-				    String dep = dependencies.get(j).getDependent().getText();
-				    String depText = rel + "(" + gov + "," + dep + ")";
-				    depList.add(depText);
-				  }
-				  
-				  indexMap.put("dependencies", depList);
-				}
+      String id = testDoc.getId();
 
-				SolrInputDocument solrInpDoc = this.wrapper
-						.buildSolrDocument(indexMap);
-				String docXML = this.wrapper.convertSolrDocInXML(solrInpDoc);
+      ArrayList<Sentence> sentenceList = Utils.fromFSListToCollection(testDoc.getSentenceList(),
+              Sentence.class);
 
-				// System.out.println(docXML);
+      // ArrayList<String>synonymList=new ArrayList<String>();
+      for (int i = 0; i < sentenceList.size(); i++) {
+        Sentence sent = sentenceList.get(i);
+        String sentText = sent.getText();
+        String sentId = id + "_" + i;
+        HashMap<String, Object> indexMap = new HashMap<String, Object>();
+        indexMap.put("docid", id);
+        indexMap.put("id", sentId);
+        indexMap.put("text", sentText);
 
-				this.wrapper.indexDocument(docXML);
-				// this.wrapper.indexCommit();
-				this.wrapper.getServer().commit();
+        FSList fsNounList = sent.getPhraseList();
+        ArrayList<NounPhrase> nounPhrases = Utils.fromFSListToCollection(fsNounList,
+                NounPhrase.class);
+        ArrayList<String> nnList = new ArrayList<String>();
+        for (int j = 0; j < nounPhrases.size(); j++) {
+          // NounPhrase nnPhr=nounPhrases.get(j);
+          nnList.add(nounPhrases.get(j).getText());
+          /*
+           * ArrayList<Synonym>synList=Utils.fromFSListToCollection(nnPhr.getSynonyms(),Synonym.class
+           * ); for(int k=0;k<synList.size();k++){ synonymList.add(synList.get(k).getText()); }
+           */
+        }
 
-			}
+        indexMap.put("nounphrases", nnList);
 
-		} catch (Exception e) {
-			//System.out.println(testDoc);
-			e.printStackTrace();
-		}
+        FSList fsNEList = sent.getNerList();
+        ArrayList<NER> namedEntities = Utils.fromFSListToCollection(fsNEList, NER.class);
+        ArrayList<String> neList = new ArrayList<String>();
+        for (int j = 0; j < namedEntities.size(); j++) {
+          // NER ner=namedEntities.get(j);
+          neList.add(namedEntities.get(j).getText());
+          /*
+           * ArrayList<Synonym>synList=Utils.fromFSListToCollection(ner.getSynonyms(),Synonym.class);
+           * for(int k=0;k<synList.size();k++){ synonymList.add(synList.get(k).getText()); }
+           */
+        }
+        indexMap.put("namedentities", neList);
+        // indexMap.put("synonyms",synonymList);
 
-	}
+        FSList fsDependencies = sent.getDependencyList();
+        if (fsDependencies != null) {
+          ArrayList<Dependency> dependencies = Utils.fromFSListToCollection(fsDependencies,
+                  Dependency.class);
+          ArrayList<String> depList = new ArrayList<String>();
+          for (int j = 0; j < dependencies.size(); j++) {
+            String rel = dependencies.get(j).getRelation();
+            String gov = dependencies.get(j).getGovernor().getText();
+            String dep = dependencies.get(j).getDependent().getText();
+            String depText = rel + "(" + gov + "," + dep + ")";
+            depList.add(depText);
+          }
+
+          indexMap.put("dependencies", depList);
+
+          FSList verbPhraseList = sent.getVerbPhraseList();
+          if (verbPhraseList != null) {
+            ArrayList<VerbPhrase> verbPhrases = Utils.fromFSListToCollection(verbPhraseList,
+                    VerbPhrase.class);
+            ArrayList<String> verbList = new ArrayList<String>();
+            for (int j = 0; j < verbPhrases.size(); j++) {
+              verbList.add(verbPhrases.get(j).getText());
+            }
+            indexMap.put("verbphrases", verbList);
+          }
+        }
+
+        SolrInputDocument solrInpDoc = this.wrapper.buildSolrDocument(indexMap);
+        String docXML = this.wrapper.convertSolrDocInXML(solrInpDoc);
+
+        // System.out.println(docXML);
+
+        this.wrapper.indexDocument(docXML);
+        // this.wrapper.indexCommit();
+        this.wrapper.getServer().commit();
+
+      }
+
+    } catch (Exception e) {
+      // System.out.println(testDoc);
+      e.printStackTrace();
+    }
+
+  }
 }
